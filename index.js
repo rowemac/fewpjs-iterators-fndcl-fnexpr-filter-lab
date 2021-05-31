e9171540132c87b1629cf4e62fc4d63ab4ab8af6
Mac Rowe <m.rowe42@gmail.com>
2021-05-31 10:43:58 -0400

    commit tree 04da56379aae02419bbeade31264b91aaef1e2b6

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    let array = [];
+    drivers.filter(driver => {
+        if (driver.toLowerCase() === string.toLowerCase()) {
+            array.push(driver);
+        };
+    });
+    return array; 
+};
+
+function fuzzyMatch(drivers, string) {
+    let array = [];
+    drivers.filter(driver => {
+        if (driver.slice(0, 2) === string ) {
+            array.push(driver);
+        };
+    });
+    return array;
+};
+
+function matchName(drivers, string) {
+    let array = [];
+    drivers.filter(driver => {
+        if (driver.name === string) {
+            array.push(driver);
+        };
+    });
+    return array; 
+};
